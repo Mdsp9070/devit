cb842f0eafd5affc93d888886980532c548cf169
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-05-12 08:01:45 -0300

    rewrote `create_article` function

--- lib/devit/client.ex
+++ lib/devit/client.ex
@@ -14,23 +14,58 @@ defmodule Devit.Client do
     ]
   end
 
+  @spec create_article(String.t(), String.t()) ::
+          {:ok, String.t()} | {:error, atom() | String.t()}
+  def create_article(api_key, body) do
+    headers = [{"content-type", "application/json"} | base_headers(api_key)]
+
+    :post
+    |> Finch.build(article_url(), headers, body)
+    |> Finch.request(__MODULE__)
+    |> handle_articles_response()
+  end
+
+  defp handle_articles_response({:ok, %Response{status: 201, body: body}}) do
+    article_url =
+      body
+      |> Jason.decode!()
+      |> Map.get("url")
+
+    {:ok, article_url}
+  end
+
+  defp handle_articles_response({:ok, %Response{status: 422, body: body}}) do
+    reason =
+      body
+      |> Jason.decode!()
+      |> Map.get("error")
+
+    {:error, reason}
+  end
+
+  defp handle_articles_response({:error, _reason} = err), do: err
+  defp handle_articles_response({:ok, %Response{status: 400}}), do: {:error, :bad_request}
+  defp handle_articles_response({:ok, %Response{status: 401}}), do: {:error, :unauthorized}
+  defp handle_articles_response({:ok, %Response{status: 403}}), do: {:error, :forbidden}
+  defp handle_articles_response({:ok, %Response{status: 429}}), do: {:error, :many_requests}
+
   @spec ping_user(String.t()) :: {:ok, :valid_user} | {:error, atom()}
   def ping_user(api_key) do
     :get
-    |> Finch.build(user_url(), headers(api_key))
+    |> Finch.build(user_url(), base_headers(api_key))
     |> Finch.request(__MODULE__)
     |> handle_user_response()
   end
 
+  defp handle_user_response({:error, _reason} = err), do: err
   defp handle_user_response({:ok, %Response{status: 200}}), do: {:ok, :valid_user}
   defp handle_user_response({:ok, %Response{status: 401}}), do: {:error, :unauthenticated}
   defp handle_user_response({:ok, %Response{status: _}}), do: {:error, :invalid_api_key}
-  defp handle_user_response({:error, _reason} = err), do: err
 
   defp pool_size, do: 2
 
   defp base_url, do: "https://dev.to/api"
   defp user_url, do: base_url() <> "/users/me"
   defp article_url, do: base_url() <> "/articles"
-  defp headers(api_key), do: [{"api_key", api_key}]
+  defp base_headers(api_key), do: [{"api_key", api_key}]
 end
